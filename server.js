629c7941844af7e34b5916ba19fac8f4a0fce3c4
Paulo Ricardo Alves Campos <62405013+PauloRicardoDeveloper@users.noreply.github.com>
2020-04-16 00:04:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json
 
 const express = require("express");
 const app = express();
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("./"));
-
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile("index.html");
 });
 
 // send the default array of dreams to the webpage
 app.get("/registrar", (request, response) => {
-  // express helps us take JS objects and send them as JSON
   response.sendFile("./register.html");
 });
 
@@ -30,12 +19,12 @@ var http = require("https");
 var ProxyAgent = require("proxy-agent");
 
 // HTTP, HTTPS, or SOCKS proxy to use
-var proxyUri = process.env.https_proxy || "https://52.72.251.198:3000";
+var proxyUri = process.env.https_proxy || "https://ipofwebsite:port";
 
 var opts = {
-  method: "POST",
-  host: ".net",
-  path: "./",
+  method: "GET",
+  host: "host",
+  path: "path",
   // this is the important part!
   agent: new ProxyAgent(proxyUri)
 };
